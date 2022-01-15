d70fbdad737877b57816c393176a27aa20d901ae
TRahulT <gujjarrahul716@gmail.com>
2022-01-15 11:06:26 +0530

    first day

---
+++ basics.py
@@ -0,0 +1,37 @@
+# first program to print any thing
+# we use print to display output
+print("I love music")
+
+#basic arithmatic operations 
+# here we assigned the values in the beg 
+a=50
+b=4
+#to add numbers
+print(a+b)
+#to substract
+print(a-b)
+# to multiply
+print(a*b)
+#to divide
+print(a/b)
+#(**)for power operation
+print(a**b)
+#(%) we use to get reminder
+print(a%b)
+#// to get modulus values
+print(a//b)
+
+
+#if we want take input from user we can use input() method
+#it will only contain string
+A=input("enter the value of A:")
+print(A) 
+# we use type casting to take input()
+A1=int(input("enter value"))
+print(A1)
+#float we use for floating type values
+#bool for boolean type
+#int for integerr type 
+
+#we can also find the tpe of any data type 
+print(type(A1))
