d840f60751f50535120dd7ccc89a86e7a0b7bdd6
TRahulT <89895996+TRahulT@users.noreply.github.com>
2022-02-06 23:20:30 +0530

    bubble sort

--- bubblesort.py
+++ bubblesort.py
@@ -2,6 +2,6 @@ num= int(input("no. of element"))
 list1=[int(input("enter the element "))for value in range(num)]
 for i in range(len(list1)-1):
     for j in range(i+1,len(list1)):
-        if list1[j]<list1[i]:
+        if list1[j]>list1[i]:
             list1[i],list1[j]=list1[j],list1[i]
 print(list1)
